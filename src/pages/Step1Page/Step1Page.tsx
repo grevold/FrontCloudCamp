ca8b782b91c9d81b5d6b34ab94270dd87b422ece
Alexey Grevtsov <grevold@yandex.ru>
2023-06-11 18:42:29 +0500

    Добавил прогресс-бар

--- src/pages/Step1Page/Step1Page.tsx
+++ src/pages/Step1Page/Step1Page.tsx
@@ -1,11 +1,13 @@
 import { Container } from "../../components/Container/Container";
 import { FormContainer } from "../../components/FormContainer/FormContainer";
+import { ProgressBar } from "../../components/ProgressBar/ProgressBar";
 import { ButtonsNavigation } from "./components/ButtonsNavigation/ButtonsNavigation";
 
 export function Step1Page() {
   return (
     <Container>
       <FormContainer>
+        <ProgressBar currentValue={4} maxValue={4} />
         <h1>Step1Page</h1>
         <ButtonsNavigation />
       </FormContainer>
