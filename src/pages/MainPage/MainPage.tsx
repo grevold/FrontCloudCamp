c5a0633764ae51fc1f08e24242bf8a483eb4ac11
Алексей <87669945+grevold@users.noreply.github.com>
2023-06-11 17:58:15 +0500

    Добавил хедер на главную страницу (#12)

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,13 +1,14 @@
 import { Container } from "../../components/Container/Container";
 import { StyledLink } from "../../components/StyledLink/StyledLink";
 import { RoutePath } from "../../types";
+import { Header } from "./components/Header/Header";
 
 import s from "./MainPage.module.css";
 
 export function MainPage() {
   return (
     <Container className={s.root}>
-      <h1>main page</h1>
+      <Header />
       <StyledLink to={RoutePath.Step1Page}>Начать</StyledLink>
     </Container>
   );
