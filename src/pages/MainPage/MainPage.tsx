2b921421a41673bfb0e975b5b5f7f20af0dabaf6
Алексей <87669945+grevold@users.noreply.github.com>
2023-06-11 17:16:19 +0500

    Добавил шрифты (#11)

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,5 +1,6 @@
 import { Container } from "../../components/Container/Container";
 import { StyledLink } from "../../components/StyledLink/StyledLink";
+import { RoutePath } from "../../types";
 
 import s from "./MainPage.module.css";
 
@@ -7,7 +8,7 @@ export function MainPage() {
   return (
     <Container className={s.root}>
       <h1>main page</h1>
-      <StyledLink to="/step1">Начать</StyledLink>
+      <StyledLink to={RoutePath.Step1Page}>Начать</StyledLink>
     </Container>
   );
 }
