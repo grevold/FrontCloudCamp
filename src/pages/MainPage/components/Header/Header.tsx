217060d94e6b9c95df184311a0205210a017ea00
Alexey Grevtsov <grevold@yandex.ru>
2023-06-11 17:56:39 +0500

    Добавил хедер на главную страницу

---
+++ src/pages/MainPage/components/Header/Header.tsx
@@ -0,0 +1,30 @@
+import { FolderIcon } from "../../../../icons/FolderIcon";
+import s from "./Header.module.css";
+
+export function Header() {
+  return (
+    <div className={s.root}>
+      <div className={s.container}>
+        <div className={s.avatar}>АИ</div>
+        <div className={s.userInfo}>
+          <h1 className={s.header}>Иван Иванов</h1>
+          <ul className={s.listFolder}>
+            <li className={s.listFolderItem}>
+              <FolderIcon className={s.folderIcon} />
+              <span>Telegram</span>
+            </li>
+            <li className={s.listFolderItem}>
+              <FolderIcon className={s.folderIcon} />
+              <span>GitHub</span>
+            </li>
+            <li className={s.listFolderItem}>
+              <FolderIcon className={s.folderIcon} />
+              <span>Resume</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <hr className={s.hr} />
+    </div>
+  );
+}
