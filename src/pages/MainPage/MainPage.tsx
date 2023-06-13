7f1852aeda733ace472c20e4a0e2d4a2da24e916
Алексей <87669945+grevold@users.noreply.github.com>
2023-06-13 20:01:37 +0500

    Добавил форму sign-in (#14)
    * Добавил форму sign-in
    
    * Добавил форму sign-in

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,7 +1,6 @@
 import { Container } from "../../components/Container/Container";
-import { StyledLink } from "../../components/StyledLink/StyledLink";
-import { RoutePath } from "../../types";
 import { Header } from "./components/Header/Header";
+import { SignInForm } from "./components/SignInForm/SignInForm";
 
 import s from "./MainPage.module.css";
 
@@ -9,7 +8,10 @@ export function MainPage() {
   return (
     <Container className={s.root}>
       <Header />
-      <StyledLink to={RoutePath.Step1Page}>Начать</StyledLink>
+      <SignInForm
+        onClick={() => console.log("Hello, world!")}
+        onSubmit={() => console.log("Hello, world!")}
+      />
     </Container>
   );
 }
