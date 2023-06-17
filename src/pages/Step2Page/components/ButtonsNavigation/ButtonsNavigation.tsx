ec5849bb3a2d22297f81267c4d3fbefdc7dc8222
Alexey Grevtsov <grevold@yandex.ru>
2023-06-17 23:22:08 +0500

    Реализовал валидацию для формы на шаге2

--- src/pages/Step2Page/components/ButtonsNavigation/ButtonsNavigation.tsx
+++ src/pages/Step2Page/components/ButtonsNavigation/ButtonsNavigation.tsx
@@ -3,13 +3,19 @@ import { RoutePath } from "../../../../types";
 
 import s from "./ButtonsNavigation.module.css";
 
-export function ButtonsNavigation() {
+interface Props {
+  disabled?: boolean;
+}
+
+export function ButtonsNavigation({ disabled }: Props) {
   return (
     <div className={s.root}>
       <StyledLink to={RoutePath.Step1Page} mod={"secondary"}>
         Назад
       </StyledLink>
-      <StyledLink to={RoutePath.Step3Page}>Далее</StyledLink>
+      <StyledLink to={RoutePath.Step3Page} disabled={disabled}>
+        Далее
+      </StyledLink>
     </div>
   );
 }
--- src/pages/Step2Page/components/FormStep2/useFormStep2.ts
+++ src/pages/Step2Page/components/FormStep2/useFormStep2.ts
@@ -1,7 +1,8 @@
 import { useForm } from "react-hook-form";
-import { object, string } from "yup";
+import { array, object, string } from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { texts } from "../../../../texts";
+import { useState } from "react";
 
 // Значения, которые мы будем доставать из формы.
 export interface FormValues {
@@ -13,9 +14,9 @@ export interface FormValues {
 const { advantages, checkBox, radio } = texts.Errors.Step2;
 
 const formSchema = object().shape({
-  advantages: string().required(advantages.required),
-  checkBox: string().required(checkBox.required),
-  radio: string().required(radio.required),
+  advantages: array().of(string().min(1, "advantage не должен быть пустым")),
+  checkBox: array().min(1, "Нужно выбрать хотя бы один checkBox-элемент"),
+  radio: string().required("Нужно выбрать хотя бы один radio-элемент"),
 });
 
 export const useFormStep2 = (
@@ -26,10 +27,35 @@ export const useFormStep2 = (
     // @ts-ignore
     resolver: yupResolver(formSchema),
   });
+  const [advantages, setAdvantages] = useState<string[]>([""]);
+
+  function removeElementFromArrayByIndex<T>(array: T[], index: number): T[] {
+    const result = [];
+    for (let i = 0; i < array.length; i++) {
+      if (i !== index) {
+        result.push(array[i]);
+      }
+    }
+    return result;
+  }
+
+  const handleAddAdvantage = () =>
+    setAdvantages((advantages) => [...advantages, ""]);
+  const handleRemoveAdvantage = (index: number) =>
+    setAdvantages((advantages) =>
+      removeElementFromArrayByIndex(advantages, index)
+    );
 
   const submit = handleSubmit(({ advantages, checkBox, radio }: FormValues) =>
     onSubmit(advantages, checkBox, radio)
   );
 
-  return { submit, register, formState };
+  return {
+    submit,
+    register,
+    formState,
+    advantages,
+    handleAddAdvantage,
+    handleRemoveAdvantage,
+  };
 };
