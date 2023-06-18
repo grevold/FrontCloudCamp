9c15de9d2b12984d7623ed504fcaefc4d3479e3d
Alexey Grevtsov <grevold@yandex.ru>
2023-06-18 16:40:34 +0500

    Исправил баг на шаге2

---
+++ src/pages/Step2Page/components/AdvantagesForm/AdvantagesForm.tsx
@@ -0,0 +1,34 @@
+import { FormState, UseFormRegister } from "react-hook-form";
+import { FormStep2Values } from "../../../../types";
+import { AdvantageInput } from "../AdvantageInput/AdvantageInput";
+import { useAdvantagesForm } from "./useAdvantagesForm";
+
+import s from "./AdvantagesForm.module.css";
+
+interface Props {
+  register: UseFormRegister<FormStep2Values>;
+  formState: FormState<FormStep2Values>;
+}
+
+export const AdvantagesForm: React.FC<Props> = ({ register, formState }) => {
+  const { advantages, handleRemoveAdvantage, handleAddAdvantage } =
+    useAdvantagesForm();
+
+  return (
+    <div className={s.root}>
+      <h1 className={s.header}>Advantages</h1>
+      {advantages.map((_, index) => (
+        <AdvantageInput
+          key={index}
+          fieldName={`advantages.${index}`}
+          onRemove={() => handleRemoveAdvantage(index)}
+          register={register}
+          errorMessage={formState.errors?.advantages?.[index]?.message}
+        />
+      ))}
+      <button className={s.addAdvantages} onClick={handleAddAdvantage}>
+        +
+      </button>
+    </div>
+  );
+};
--- src/pages/Step2Page/components/FormStep2/useFormStep2.ts
+++ src/pages/Step2Page/components/FormStep2/useFormStep2.ts
@@ -2,14 +2,13 @@ import { useForm } from "react-hook-form";
 import { array, object, string } from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { texts } from "../../../../texts";
-import { useState } from "react";
 import { FormStep2Values } from "../../../../types";
 import { useAppSelector } from "../../../../store/store";
 
 const { advantages, checkBox, radio } = texts.Errors.Step2;
 
 const formSchema = object().shape({
-  advantages: array().of(string().min(1, "advantage не должен быть пустым")),
+  advantages: array().of(string()),
   checkBox: array()
     .min(1, "Нужно выбрать хотя бы один checkBox-элемент")
     .required("Нужно выбрать хотя бы один checkBox-элемент"),
@@ -29,34 +28,10 @@ export const useFormStep2 = (
       ...userStore.formStep2Values,
     },
   });
-  const [advantages, setAdvantages] = useState<string[]>(
-    userStore.formStep2Values ? userStore.formStep2Values.advantages : []
-  );
-
-  function removeElementFromArrayByIndex<T>(array: T[], index: number): T[] {
-    const result = [];
-    for (let i = 0; i < array.length; i++) {
-      if (i !== index) {
-        result.push(array[i]);
-      }
-    }
-    return result;
-  }
-
-  const handleAddAdvantage = () =>
-    setAdvantages((advantages) => [...advantages, ""]);
-
-  const handleRemoveAdvantage = (index: number) =>
-    setAdvantages((advantages) =>
-      removeElementFromArrayByIndex(advantages, index)
-    );
 
   return {
     handleSubmit: handleSubmit(onSubmit),
     register,
     formState,
-    advantages,
-    handleAddAdvantage,
-    handleRemoveAdvantage,
   };
 };
